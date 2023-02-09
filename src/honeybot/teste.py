569dbb442b73c669baea48787134593440246372
Paulo Ferraz <paulosgf.pf@gmail.com>
2023-02-09 16:10:26 -0300

    14 commit

---
+++ src/honeybot/teste.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import time
+
+
+def main():
+    print(time.strftime("%Y.%m.%d"))
+    print(os.getcwd())
+    print(math.sqrt(4))
+
+
+if __name__ == "__main__":
+    main()
